2bbfb4abced7a6fc217a3de4a29fbaed08a3bc56
Claudia <claui@users.noreply.github.com>
2019-05-25 14:15:56 +0200

    macos-sdk-headers: accommodate differences among minor or beta releases

--- Casks/macos-sdk-headers.rb
+++ Casks/macos-sdk-headers.rb
@@ -1,6 +1,6 @@
 cask 'macos-sdk-headers' do
-  version '10.2.1.1.1.1554506761'
-  sha256 'de751905853dc3765962abe8e9aa45a8c4080a414ba603cfd4dd32c45aae455c'
+  version :latest
+  sha256 :no_check
 
   url 'file:///Library/Developer/CommandLineTools/Packages/macOS_SDK_headers_for_macOS_10.14.pkg'
   name 'macOS SDK headers for macOS 10.14'
